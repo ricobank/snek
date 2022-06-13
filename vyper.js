d125f5d9eddeeeab5fc53ddc116490732c9d1a8b
dmfxyz <d@dmf.xyz>
2022-06-13 18:45:07 -0400

    capture outputed abis and bytecodes from make command

--- vyper.js
+++ vyper.js
@@ -2,11 +2,15 @@ const execSync = require('node:child_process').execSync
 const process = require('node:process')
 
 module.exports = vy = {}
+const outs = []
 
 vy.compile = (path, build_test=false) => {
     try {
         const stdout = execSync(`vyper -f abi,bytecode ${path}`, { encoding: 'utf8' })
-        console.log(stdout)
+        compilation_results = stdout.split("\n").slice(0, -1).forEach((element, index, array) => {
+            if (index % 2 == 0) outs.push({abi: element, bytecode: array[index+1]})
+        });
+        console.log(outs)
     } catch (err) {
         const { status, stderr } = err;
         if (status > 0 || (stderr && stderr.toLowerCase().includes('warning'))) {
