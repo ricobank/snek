2c32325fd52c289cc89a56a997410d199f823adb
dmfxyz <d@dmf.xyz>
2022-06-15 22:34:50 -0400

    clean up source contract parsing

--- src/runner.js
+++ src/runner.js
@@ -12,14 +12,12 @@ runner.run = async (output_dir) => {
     const dapp = await dpack.load(multifab_pack, ethers, signer)
     const multifab = await dapp._types.Multifab.deploy()
     const src_output = require(resolve(`${output_dir}/SrcOutput.json`))
-    const src_contracts = Object.values(src_output.contracts)
+    const src_contracts = Object.values(src_output.contracts).map((obj) => Object.entries(obj)[0])
     const deploy_info = {}
     // Add Src Contracts to Multifab
     // not forEach because need to have all promises resolve
-    for (contract of src_contracts) {
-        const contract_name = Object.keys(contract)[0]
-        const bytecode = contract[contract_name].evm.bytecode.object
-        const cache_tx = await send(multifab.cache, bytecode);
+    for ([contract_name, contract] of src_contracts) {
+        const cache_tx = await send(multifab.cache, contract.evm.bytecode.object);
         [,codehash] = cache_tx.events.find(event => event.event === 'Added').args
         deploy_info[contract_name] = { codehash: codehash}
     }
