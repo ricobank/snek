90c0accd55fc083ce194e366a914f39ac5ff0f3f
stobiewan <getsforwarded@gmail.com>
2022-06-16 23:56:26 +1200

    fix finding snek.vy path, remove try hiding errors

--- src/vyper.js
+++ src/vyper.js
@@ -1,5 +1,4 @@
 const execSync = require('node:child_process').execSync
-const process = require('node:process')
 const fs = require('fs')
 
 module.exports = vy = {}
@@ -32,20 +31,12 @@ vy._generate_json = (files, path, outputDir, id) => {
 vy.compile = (path, outputDir, outputId) => {
     console.log(`Compiling contracts in ${path} to ${outputDir}`)
     const files = []
-    try {
-        if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir)
-        if (fs.lstatSync(path).isFile()) files.push(path)
-        else if (fs.lstatSync(path).isDirectory()) files.push(...(fs.readdirSync(path)))
-        else throw `Invalid path ${path}. Path to contracts must be file or directory.`
+    if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir)
 
-        vy._generate_json(files, path, outputDir, outputId)
-        execSync(`vyper-json ${outputDir}/${outputId}Input.json -o ${outputDir}/${outputId}Output.json`, {encoding: 'utf-8'})
-    } catch (err) {
-        const { status, stderr } = err;
-        if (status > 0 || (stderr && stderr.toLowerCase().includes('warning'))) {
-            console.error('Failed due to:');
-            console.error(stderr);
-            process.exit(1);
-        }
-    }
+    if (fs.lstatSync(path).isFile()) files.push(path)
+    else if (fs.lstatSync(path).isDirectory()) files.push(...(fs.readdirSync(path)))
+    else throw `Invalid path ${path}. Path to contracts must be file or directory.`
+
+    vy._generate_json(files, path, outputDir, outputId)
+    execSync(`vyper-json ${outputDir}/${outputId}Input.json -o ${outputDir}/${outputId}Output.json`, {encoding: 'utf-8'})
 }
